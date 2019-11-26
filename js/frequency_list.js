9121198293ceba673114843bf929f48499ce5f69
birong <birong.zhou@kaiostech.com>
2019-11-26 10:50:27 +0800

    Bug 76424 - Restore frequency name after remove favorite from frequency. r=Weiwei.Wu & haoqing.mao

--- js/frequency_list.js
+++ js/frequency_list.js
@@ -91,6 +91,9 @@
       frequency = this.getFrequencyByElement(e.rangeParent);
       element = e.rangeParent;
     }
+    if (!favorite) {
+      FrequencyManager.updateFrequencyName(frequency, frequency.toFixed(1));
+    }
     FrequencyManager.updateFrequencyFavorite(frequency, favorite);
     if (StatusManager.status === StatusManager.STATUS_FAVORITE_SHOWING) {
       this.updateFavoriteListUI();
